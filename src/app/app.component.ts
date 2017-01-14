d648b445a69a7d6f5d82d94a675ffbe849df22d4
Bram Borggreve <borggreve@gmail.com>
2017-01-14 01:13:37 -0500

    Add Bootstrap and base layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>
-    {{title}}
-  </h1>
-  <router-outlet></router-outlet>
+  <div class="container">
+    <nav class="my-3 navbar navbar-toggleable-md navbar-light bg-faded">
+      <a class="navbar-brand" href="#">{{title}}</a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-outlet></router-outlet>
+  </div>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular Form Exercise';
 }
